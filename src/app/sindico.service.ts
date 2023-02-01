7fb6d9685e7f60e037548d3ec1df78877fe0c36d
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-02-01 09:54:10 -0300

    Config básica do sindico.service

--- src/app/sindico.service.ts
+++ src/app/sindico.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class SindicoService {
+  private apiServeUrl = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public listaSindicos(): Observable<any> {
+    return this.http.get<any>(`${this.apiServeUrl}/sindicos`);
+  }
 }
