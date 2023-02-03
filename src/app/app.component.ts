a67cde792b85c7db700b15514dfcc541e2db283a
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-02-03 11:39:53 -0300

    Configurações de Componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Sindico } from './sindico';
+import { SindicoService } from './sindico.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sindicon';
+  public sindicos: Sindico[] | any;
+
+  constructor (private sindicoService: SindicoService){}
+
+  ngOnInit() {
+    
+  }
+  
+  public listarSindicos(): void {
+    this.sindicoService.listaSindicos().subscribe(
+      (response: Sindico[]) => {
+        this.sindicos = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SindicoService } from './sindico.service';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [SindicoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
