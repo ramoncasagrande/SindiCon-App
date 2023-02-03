d83e13bec71aac560a05bb53e91c2dbb7fd340a2
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-02-03 11:53:07 -0300

    Adicionado HttpClientModule no appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { SindicoService } from './sindico.service';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [SindicoService],
   bootstrap: [AppComponent]
